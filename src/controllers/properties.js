ffc817fa53892bdc5647ca62d52179d24a4fcfd5
Manzi Roger <hasua.mr@gmail.com>
2023-07-16 18:41:38 +0200

    added management portal

--- src/controllers/properties.js
+++ src/controllers/properties.js
@@ -1,7 +1,7 @@
 import db from "../db"
 
 const save = (obj) => {
-    let query = `INSERT INTO properties SET brief_description='${obj.brief_description}',property_usage_type='${obj.property_usage_type}',description='${obj.description}',photos='${obj.photos}',rooms='${obj.rooms}',bathroom='${obj.bathroom}',parking_slots='${obj.parking_slots}',sqm='${obj.sqm}',year_built='${obj.year_built}',price='${obj.price}',address='${obj.address}',status='${obj.status}'`;
+    let query = `INSERT INTO properties SET code='${obj.code}',brief_description='${obj.brief_description}',property_usage_type='${obj.property_usage_type}',description='${obj.description}',photos='${obj.photos}',rooms='${obj.rooms}',bathroom='${obj.bathroom}',parking_slots='${obj.parking_slots}',sqm='${obj.sqm}',year_built='${obj.year_built}',price='${obj.price}',address='${obj.address}',status='${obj.status}'`;
     return new Promise((resolve, reject) => {
         db.query(query, (err, res) => {
             if (err) reject(err);
@@ -42,7 +42,7 @@ const update = (id, obj) => {
         db.query(queryId, (errId, resId) => {
             if (errId) reject(errId);
             if (resId.length > 0) {
-                let query = `UPDATE properties SET brief_description='${obj.brief_description}',property_usage_type='${obj.property_usage_type}',description='${obj.description}',photos='${obj.photos}',rooms='${obj.rooms}',bathroom='${obj.bathroom}',parking_slots='${obj.parking_slots}',sqm='${obj.sqm}',year_built='${obj.year_built}',price='${obj.price}',address='${obj.address}',status='${obj.status}' WHERE id='${id}'`;
+                let query = `UPDATE properties SET code='${obj.code}',brief_description='${obj.brief_description}',property_usage_type='${obj.property_usage_type}',description='${obj.description}',photos='${obj.photos}',rooms='${obj.rooms}',bathroom='${obj.bathroom}',parking_slots='${obj.parking_slots}',sqm='${obj.sqm}',year_built='${obj.year_built}',price='${obj.price}',address='${obj.address}',status='${obj.status}' WHERE id='${id}'`;
                 db.query(query, (err, res) => {
                     if (err) reject(err)
                     resolve({status: true, message: "Property updated successfully"});
--- src/routes/viewsRouter.js
+++ src/routes/viewsRouter.js
@@ -19,10 +19,34 @@ viewRouter.get("/v/about",async (req,res)=>{
     res.sendFile(`${views_path}/about.html`);
 })
 viewRouter.get("/v/login",async (req,res)=>{
-    res.sendFile(`${views_path}/login.html`);
+    res.sendFile(`${views_path}/pages-login.html`);
+})
+viewRouter.get("/v/register",async (req,res)=>{
+    res.sendFile(`${views_path}/page-register.html`);
 })
 viewRouter.get("/v/contact",async (req,res)=>{
     res.sendFile(`${views_path}/contact.html`);
+})
+viewRouter.get("/v/properties",async (req,res)=>{
+    res.sendFile(`${views_path}/in-properties.html`);
+})
+viewRouter.get("/v/users",async (req,res)=>{
+    res.sendFile(`${views_path}/in-users.html`);
+})
+viewRouter.get("/v/bids",async (req,res)=>{
+    res.sendFile(`${views_path}/in-bids.html`);
+})
+
+viewRouter.get("/v/sales",async (req,res)=>{
+    res.sendFile(`${views_path}/in-sales.html`);
+})
+viewRouter.get("/v/profile",async (req,res)=>{
+    res.sendFile(`${views_path}/users-profile.html`);
+})
+
+
+viewRouter.get("/v/add-property",async (req,res)=>{
+    res.sendFile(`${views_path}/add-property.html`);
 })
 
 export default viewRouter;
